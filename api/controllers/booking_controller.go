840318183404d436d6b26c6802d72c2dd009782a
orudda <othavio.rudda.cc@gmail.com>
2023-10-28 18:04:15 -0300

    booking working

--- api/controllers/booking_controller.go
+++ api/controllers/booking_controller.go
@@ -11,11 +11,15 @@ import (
 )
 
 type BookingController struct {
+	ClassService   *services.ClassService
 	BookingService *services.BookingService
 }
 
-func NewBookingController(bookingService *services.BookingService) *BookingController {
-	return &BookingController{BookingService: bookingService}
+func NewBookingController(classService *services.ClassService, bookingService *services.BookingService) *BookingController {
+	return &BookingController{
+		ClassService:   classService,
+		BookingService: bookingService,
+	}
 }
 
 func (c *BookingController) CreateBooking(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +29,24 @@ func (c *BookingController) CreateBooking(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// Check if a class is available on the requested date
+	classAvailable := c.isClassAvailable(newBooking.Date)
+	if !classAvailable {
+		utils.RespondWithError(w, http.StatusBadRequest, "Class not available on the requested date")
+		return
+	}
+
 	c.BookingService.CreateBooking(newBooking)
 	utils.RespondWithJSON(w, http.StatusCreated, newBooking)
+}
+
+func (c *BookingController) isClassAvailable(date utils.CustomTime) bool {
+	classes := c.ClassService.GetClasses()
+	for _, class := range classes {
+		if date.Equal(class.StartDate.Time) || (date.After(class.StartDate.Time) && date.Before(class.EndDate.Time)) {
+			// The class exists for the requested date
+			return true
+		}
+	}
+	return false
 }
