ed26af4f62b465651f728744cc174ae98c9bf7b6
orudda <othavio.rudda.cc@gmail.com>
2023-10-29 09:05:11 -0300

    unit tests added

--- api/tests/booking_test.go
+++ api/tests/booking_test.go
@@ -11,6 +11,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var date, _ = time.Parse("2006-01-02", "2023-12-01")
+var booking = models.Booking{
+	ID:         1,
+	ClassID:    0,
+	MemberName: "Test Member",
+	Date:       utils.CustomTime{Time: date},
+}
+var class = models.Class{
+	ID:        0,
+	Name:      "Test Name",
+	StartDate: utils.CustomTime{Time: date},
+	EndDate:   utils.CustomTime{Time: date},
+	Capacity:  10,
+}
+
 func TestBooking(t *testing.T) {
 	booking := models.Booking{
 		MemberName: "Test Member",
@@ -22,12 +37,36 @@ func TestBooking(t *testing.T) {
 }
 
 func TestCreateBooking(t *testing.T) {
-	bookingService := services.DBBookingService()
-	booking := models.Booking{
-		MemberName: "Test Member",
-		Date:       utils.CustomTime{Time: time.Now()},
-	}
+	classService := services.DBClassService()
+	bookingService := services.DBBookingService(classService)
 
-	bookingService.CreateBooking(booking)
+	_ = classService.CreateClass(class)
+	_ = bookingService.CreateBooking(booking)
 	assert.Len(t, bookingService.Bookings, 1)
+}
+
+func TestUpdateBooking(t *testing.T) {
+	classService := services.DBClassService()
+	bookingService := services.DBBookingService(classService)
+	_ = classService.CreateClass(class)
+	_ = bookingService.CreateBooking(booking)
+
+	booking.MemberName = "Updated Test Mamber"
+
+	_ = bookingService.UpdateBooking(booking)
+
+	actualBooking := bookingService.GetBookings()
+
+	// Ensure that no bookings were created
+	assert.Equal(t, "Updated Test Mamber", (*actualBooking)[0].MemberName)
+}
+
+func TestDeleteBooking(t *testing.T) {
+	classService := services.DBClassService()
+	bookingService := services.DBBookingService(classService)
+
+	_ = classService.CreateClass(class)
+	_ = bookingService.CreateBooking(booking)
+	_ = bookingService.DeleteBooking(booking.ID)
+	assert.Len(t, bookingService.Bookings, 0)
 }
--- api/tests/class_test.go
+++ api/tests/class_test.go
@@ -5,6 +5,7 @@ import (
 	"api/models"
 	"api/services"
 	"api/utils"
+	"errors"
 	"testing"
 	"time"
 
@@ -35,5 +36,96 @@ func TestCreateClass(t *testing.T) {
 
 	err := classService.CreateClass(class)
 	assert.NoError(t, err)
-	assert.Len(t, classService.GetClasses(), 1)
+	assert.Len(t, classService.GetClasses(), 8)
+}
+
+func TestGetClassByID(t *testing.T) {
+
+	classService := services.DBClassService()
+	class := models.Class{
+		Name:      "Test Class",
+		StartDate: utils.CustomTime{Time: time.Now()},
+		EndDate:   utils.CustomTime{Time: time.Now().Add(time.Hour * 24 * 7)},
+		Capacity:  10,
+	}
+
+	err := classService.CreateClass(class)
+	// Test case 1: Valid class ID
+
+	validID := 1
+	classes, err := classService.GetClassByID(validID)
+	if err != nil {
+		t.Errorf("Expected no error, got: %v", err)
+	}
+	if classes.ID != validID {
+		t.Errorf("Expected class ID %d, got: %d", validID, class.ID)
+	}
+}
+func TestGetClassByIDWithNoValidId(t *testing.T) {
+
+	classService := services.DBClassService()
+
+	// Test case 2: Invalid class ID
+	invalidID := 3
+	_, err := classService.GetClassByID(invalidID)
+	if err == nil {
+		t.Error("Expected an error, but got none")
+	}
+	if errors.Is(err, errors.New("Class not found")) {
+		t.Errorf("Expected 'Class not found' error, got: %v", err)
+	}
+}
+
+func TestClassNotAvailable(t *testing.T) {
+	classService := services.DBClassService()
+
+	err := classService.IsClassAvailable(utils.CustomTime{Time: time.Now()})
+
+	if err != nil {
+		t.Error("Expected an error, but got none")
+	}
+}
+
+func TestDecrementCapacityUnavailable(t *testing.T) {
+	classService := services.DBClassService()
+
+	err := classService.DecrementClassCapacity(0)
+
+	if err.Error() != "Class not available on the requested date" {
+		t.Errorf("expected error message, but got none")
+	}
+}
+
+func TestIncrementCapacityUnavailable(t *testing.T) {
+	classService := services.DBClassService()
+
+	err := classService.IncrementClassCapacity(100)
+
+	if err.Error() != "Class not available on the requested date to increment capacity" {
+		t.Errorf("expected error message, but got none")
+	}
+}
+func TestDeleteClassNotFound(t *testing.T) {
+	classService := services.DBClassService()
+
+	err := classService.DeleteClass(0)
+
+	if err.Error() != "Class not found" {
+		t.Errorf("expected error message, but got none")
+	}
+}
+func TestUpdateClassNotFound(t *testing.T) {
+	classService := services.DBClassService()
+	class := models.Class{
+		Name:      "Test Class",
+		StartDate: utils.CustomTime{Time: time.Now()},
+		EndDate:   utils.CustomTime{Time: time.Now().Add(time.Hour * 24 * 7)},
+		Capacity:  10,
+	}
+
+	err := classService.UpdateClass(class)
+
+	if err.Error() != "Class not found" {
+		t.Errorf("expected error message, but got none")
+	}
 }
